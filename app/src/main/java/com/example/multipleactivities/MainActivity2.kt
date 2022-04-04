589839bff046ddc1db64403f5ae1c6502b9d43a5
Victor A. Quintana Vazquez <vquintana9506@guayama.inter.edu>
2022-04-04 09:14:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/multipleactivities/MainActivity2.kt
@@ -0,0 +1,28 @@
+package com.example.multipleactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.multipleactivities.databinding.ActivityMain2Binding
+import com.example.multipleactivities.databinding.ActivityMainBinding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener{
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
+}
